986575542ce02acbc947b86646afed1fc68b4fec
edd-ie <ogendoeddie@gmail.com>
2024-05-15 15:19:31 -0300

    Static and dynamic list & dropdown menus

--- app/src/main/java/edd_ie/com/github/lists/MainActivity.java
+++ app/src/main/java/edd_ie/com/github/lists/MainActivity.java
@@ -1,12 +1,17 @@
 package edd_ie.com.github.lists;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +20,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ListView list = findViewById(R.id.list);
+        Spinner dropdown = findViewById(R.id.spinner);
+
+        ArrayList<String> data = new ArrayList<>();
+
+        data.add("New York");
+        data.add("Los Angeles");
+        data.add("Chicago");
+        data.add("Houston");
+        data.add("Philadelphia");
+        data.add("Phoenix");
+        data.add("San Antonio");
+        data.add("San Diego");
+        data.add("Dallas");
+        data.add("San John");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, data.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, dropdown.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
         });
     }
 }
